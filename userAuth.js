167c5627181d2cf6bd3b198e06ca4c7d3599b9ec
tbstbstbs <tobias@30dna.com>
2020-06-30 14:18:30 +0200

    io

--- userAuth.js
+++ userAuth.js
@@ -1,5 +1,3 @@
-// Disable user auth. Copied from the official library customization example.
-// https://github.com/nytimes/library-customization-example/blob/65a6e70240cfee31e444fa20fe74ae32daf437d9/userAuth.js
 'use strict'
 
 const md5 = require('md5')
@@ -7,11 +5,12 @@ const router = require('express-promise-router')()
 
 router.use(async (req, res) => {
   req.userInfo = {
-    email: 'tm@whoelse.ai',
-    userId: '-1',
-    analyticsUserId: false // disable GA tracking
+    email: 'demo.user@example.com',
+    userId: '10',
+    // prevents incorrect GA tracking
+    analyticsUserId: ''
   }
-
+  
   return 'next'
 })
 
